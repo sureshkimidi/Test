2fddd777f800e6f97ee801d3212a4a9c2133eb2d
sureshkimidi <sureshkimidi71@gmail.com>
2025-03-12 16:16:38 +0530

    Add files via upload

---
+++ CrudProj1Application.java
@@ -0,0 +1,13 @@
+package com.santanu;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CrudProj1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CrudProj1Application.class, args);
+	}
+
+}
---
+++ SpringBootApplication.java
@@ -0,0 +1,5 @@
+package com.santanu;
+
+public @interface SpringBootApplication {
+
+}
